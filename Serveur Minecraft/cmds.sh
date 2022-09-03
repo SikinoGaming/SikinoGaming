91c6054b68950b801e020c6f1757452d9088398d
Sikino <sikino.gaming@gmail.com>
2022-09-03 14:30:32 +0200

    Ajout des fichiers pour le 2e Episode de Programmez! Serveur Minecraft.

---
+++ Serveur Minecraft/cmds.sh
@@ -0,0 +1,38 @@
+# ----------  LINUX  ----------
+
+wget https://github.com/SikinoGaming/Minecraft-Server-Downloader/releases/latest/download/cli.py
+
+# ----------  DOCKER  ----------
+
+# Installation de Docker
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install docker.io -y
+
+# Crée l'image
+sudo docker build -t serveur-mc .
+
+# Lance l'image
+sudo docker run -d \
+-v /home/{USERNAME}/server_files:/serveur \
+-p HOST-PORT:25565 \
+--name=serveur serveur-mc
+
+# Il ne se passe rien, montrer les logs
+sudo docker logs serveur-mc
+
+# Télécharger le serveur.jar
+
+# On ne peux pas le relancer
+sudo docker rm {container id}
+
+# Accepter l'eula si pas passé par MSD
+sudo nano /home/user/server_mc
+
+# Avoir l'id du container
+sudo docker ps
+
+# Faire une commande
+sudo docker exec -it {ID DU CONTAINER} /bin/bash
+
+# Arrêter le container
+sudo docker stop serveur
